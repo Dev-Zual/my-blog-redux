f096abb725093c9e588f4558295ba113e6a3b2f4
Zual Rana <freelancerzual@gmail.com>
2022-12-07 14:28:36 +0600

    loaded blogs in dashboard

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCart from "../../components/ProductCart";
 import fetchBlogData from "../../redux/thunk/Blog/fetchBlog";
